d7e778cbc6583caf93514a4caea88356822b166f
Siri N Shetty <114021638+siri-n-shetty@users.noreply.github.com>
2022-12-21 23:03:52 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -2,11 +2,11 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppeli/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
 
-constructor () ERC20("Mango Token", "Mango") {
+constructor () ERC20("", "") {
 _mint(msg.sender, 1000000 * 10 ** decimals());
 }
 }
