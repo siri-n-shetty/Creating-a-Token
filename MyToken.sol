7736ae4924eea956dba58fd25cac796631bb214e
Siri N Shetty <114021638+siri-n-shetty@users.noreply.github.com>
2022-12-21 23:36:31 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -1,12 +1,1 @@
 // SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
-
-import "@openzeppeli/contracts/token/ERC20/ERC20.sol";
-
-contract MyToken is ERC20 {
-
-constructor () ERC20("", "") {
-_mint(msg.sender, 1000000 * 10 ** decimals());
-}
-}
